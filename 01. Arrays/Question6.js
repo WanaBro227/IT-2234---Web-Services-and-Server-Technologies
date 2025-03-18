b07c5da937ed45e102647f1429a1550244404d82
Chamara Wanasinghe <chamara.wanasinghe227@gmail.com>
2025-03-18 12:02:40 +0530

    Delete 01. Arrays directory

--- 01. Arrays/Question6.js
+++
@@ -1,8 +0,0 @@
-/*
-    Exercise 01
-    Define 10 students JSON
-    store it in an array
-    find the female students
-    find the students who are following IT course
-    find the max and average GPA among the students
-*/
